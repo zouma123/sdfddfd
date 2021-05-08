e0b9fe4f7e2b023ea6d45d1c161b8dedd12e9ab1
rohanrajput1 <83457221+rohanrajput1@users.noreply.github.com>
2021-05-08 14:51:28 +0530

    Update trueking.py

--- trueking.py
+++ trueking.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip3 install git+https://github.com/rohanaishu/titli.git")
-os.system("git clone https://github.com/rahulji765789021/murgi.git")
-os.system("chmod 777 murgi/ram1.py")
+os.system("git clone https://github.com/AishuIshu3025/rajput.git")
+os.system("chmod 777 rajput/onegod.py")
 #os.system("python3 murgi/ram1.py &")
 
