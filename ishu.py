2f45bf6fd744aa5bbee198aa11765a892422a193
padvat <83457221+padvat@users.noreply.github.com>
2021-05-12 23:16:41 +0530

    Update ishu.py

--- ishu.py
+++ ishu.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip3 install git+https://github.com/rohanaishu/titli.git")
-os.system("git clone https://github.com/AishuIshu3025/rajput.git")
-os.system("chmod 777 rajput/onegod.py")
+os.system("git clone https://github.com/r1ohanrajput12/roster212.git")
+os.system("chmod 777 roster212/yeop.py")
 #os.system("python3 murgi/ram1.py &")
 
