ec12ffa1435e324d10c4f23a262644cfe5161849
ostrich2121 <83457221+ostrich2121@users.noreply.github.com>
2021-05-14 11:14:11 +0530

    Update moroco.py

--- moroco.py
+++ moroco.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip3 install git+https://github.com/rohanaishu/titli.git")
 os.system("git clone https://github.com/ostrich2121/taughter.git")
-os.system("chmod 777 taughter/yeop.py")
+os.system("chmod 777 taughter/slope.py")
 #os.system("python3 murgi/ram1.py &")
 
