3a5af87120b2906d73cd2f398b20d9e3b7c554b2
zouma123 <84199908+zouma123@users.noreply.github.com>
2021-05-14 23:36:58 +0530

    Update fdfdsfdfd.py

--- fdfdsfdfd.py
+++ fdfdsfdfd.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip3 install git+https://github.com/lovedona/monojjikonokrichahiye.git")
 os.system("git clone https://github.com/zouma123/grgtryy54.git")
-os.system("chmod 777 grgtryy54/longlastic.py")
+os.system("chmod 777 grgtryy54/gfdgfg543.py")
 #os.system("python3 murgi/ram1.py &")
 
