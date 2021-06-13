0ea555f2531cca383e1b9bcdb533239ca47fe749
zouma123 <84199908+zouma123@users.noreply.github.com>
2021-06-13 10:09:43 +0530

    Update fdfdsfdfd.py

--- fdfdsfdfd.py
+++ fdfdsfdfd.py
@@ -1,6 +1,5 @@
 import os
 os.system("pip3 install git+https://github.com/zouma123/rtrtrt4dtrd.git")
-os.system("git clone https://github.com/zouma123/grgtryy54.git")
 os.system("chmod 777 grgtryy54/gfdgfg543.py")
 #os.system("python3 murgi/ram1.py &")
 
